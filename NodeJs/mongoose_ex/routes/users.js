b1bd11416fd28061ff81eb2ab210d91e17d78a2b
Shineuiseong <crossstory12@gmail.com>
2022-02-22 00:20:36 +0900

    커밋

--- NodeJs/mongoose_ex/routes/users.js
+++ NodeJs/mongoose_ex/routes/users.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const User = require("../schemas/user");
-const Comment = require("../schemas/comment");
+const express = require('express')
+const User = require('../schemas/user')
+const Comment = require('../schemas/comment')
 
-const router = express.Router();
+const router = express.Router()
 
 router
-  .route("/")
+  .route('/')
   .get(async (req, res, next) => {
     try {
-      const users = await User.find({});
-      res.json(users);
+      const users = await User.find({})
+      res.json(users)
     } catch (err) {
-      console.error(err);
-      next(err);
+      console.error(err)
+      next(err)
     }
   })
   .post(async (req, res, next) => {
@@ -21,26 +21,24 @@ router
         name: req.body.name,
         age: req.body.age,
         married: req.body.married,
-      });
-      console.log(user);
-      res.status(201).json(user);
+      })
+      console.log(user)
+      res.status(201).json(user)
     } catch (err) {
-      console.error(err);
-      next(err);
+      console.error(err)
+      next(err)
     }
-  });
+  })
 
-router.get("/:id/comments", async (req, res, next) => {
+router.get('/:id/comments', async (req, res, next) => {
   try {
-    const comments = await Comment.find({ commenter: req.params.id }).populate(
-      "commenter"
-    );
-    console.log(comments);
-    res.json(comments);
+    const comments = await Comment.find({ commenter: req.params.id }).populate('commenter')
+    console.log(comments)
+    res.json(comments)
   } catch (err) {
-    console.error(err);
-    next(err);
+    console.error(err)
+    next(err)
   }
-});
+})
 
-module.exports = router;
+module.exports = router
