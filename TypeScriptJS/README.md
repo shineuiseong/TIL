48f3e959a4d07b50b350965c21685beb5433023f
Shineuiseong <crossstory12@gmail.com>
2022-02-21 20:16:50 +0900

    typescript 환경설정

---
+++ TypeScriptJS/README.md
@@ -0,0 +1,1 @@
+## TypeScript을 연습하는 TypeChain(블럭체인) 프로젝트
---
+++ TypeScriptJS/src/index.ts
@@ -0,0 +1,13 @@
+
+
+// 연습
+
+const name ="euiseong",
+age = 29,
+gender="male"
+
+const sayHi = (name:string,age:number,gender:string):void =>{
+    console.log(`hello ${name}, you are ${age}, you are ${gender}`)
+}
+
+export {};
