7c290d035fd19fbb9c300d231ff962b23e26dad7
Shineuiseong <crossstory12@gmail.com>
2022-02-21 23:47:58 +0900

    createNewBlock 구현

--- TypeScriptJS/src/index.ts
+++ TypeScriptJS/src/index.ts
@@ -1,9 +1,15 @@
+
+import * as Crypto from 'crypto-js'
 class Block{
     public index : number
     public hash : string
     public previousHash : string
     public data : string
     public timestamp : number
+
+    static calculateBlockHash = (index:number, previousHash:string, timestamp:number, data:string):string=>
+    Crypto.SHA256(index+previousHash+timestamp+data).toString()
+
     constructor(index:number, hash:string, previousHash:string, data:string, timestamp : number)
     {
         this.index = index
@@ -19,7 +25,24 @@ const genesisBlock:Block = new Block(0,"2020202020202","","Hello",123456)
 
 let blockchain:[Block] = [genesisBlock]
 
-console.log(blockchain)
+const getBlockchain = ():Block[] =>blockchain
 
+const getLatesBlock = () :Block => blockchain[blockchain.length-1]
+
+const getNewTimeStamp = (): number =>Math.round(new Date().getTime()/1000)
+
+const createNewBlock = (data:string):Block =>{
+    const previosBlock: Block = getLatesBlock()
+    const newIndex :number = previosBlock.index +1
+    const newTimestamp:number = getNewTimeStamp()
+    const newHash :string = Block.calculateBlockHash(newIndex,previosBlock.hash,newTimestamp,data)
+    
+    const newBlock :Block = new Block(newIndex,newHash,previosBlock.hash,data,newTimestamp)
+
+    return newBlock
+}
+// console.log(blockchain)
+
+console.log(createNewBlock("hello"),createNewBlock('byby'))
 
 export {}
