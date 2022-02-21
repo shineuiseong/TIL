f79ccaf76e154b4bbceab97212e6ce8a1ff74d57
Shineuiseong <crossstory12@gmail.com>
2022-02-21 23:57:48 +0900

    isBlockValid 구현

--- TypeScriptJS/src/index.ts
+++ TypeScriptJS/src/index.ts
@@ -10,6 +10,10 @@ class Block{
     static calculateBlockHash = (index:number, previousHash:string, timestamp:number, data:string):string=>
     Crypto.SHA256(index+previousHash+timestamp+data).toString()
 
+    static validateStructure = (aBlock:Block):boolean=>typeof aBlock.index ==="number" &&
+     typeof aBlock.hash ==="string" && typeof aBlock.previousHash ==="string" &&
+     typeof aBlock.timestamp ==="number" && typeof aBlock.data ==="string"
+
     constructor(index:number, hash:string, previousHash:string, data:string, timestamp : number)
     {
         this.index = index
@@ -32,17 +36,37 @@ const getLatesBlock = () :Block => blockchain[blockchain.length-1]
 const getNewTimeStamp = (): number =>Math.round(new Date().getTime()/1000)
 
 const createNewBlock = (data:string):Block =>{
-    const previosBlock: Block = getLatesBlock()
-    const newIndex :number = previosBlock.index +1
+    const previousBlock: Block = getLatesBlock()
+    const newIndex :number = previousBlock.index +1
     const newTimestamp:number = getNewTimeStamp()
-    const newHash :string = Block.calculateBlockHash(newIndex,previosBlock.hash,newTimestamp,data)
+    const newHash :string = Block.calculateBlockHash(newIndex,previousBlock.hash,newTimestamp,data)
     
-    const newBlock :Block = new Block(newIndex,newHash,previosBlock.hash,data,newTimestamp)
+    const newBlock :Block = new Block(newIndex,newHash,previousBlock.hash,data,newTimestamp)
 
     return newBlock
 }
 // console.log(blockchain)
 
-console.log(createNewBlock("hello"),createNewBlock('byby'))
+// console.log(createNewBlock("hello"),createNewBlock('byby'))
+
+const getHashforBlock= (aBlock:Block):string =>Block.calculateBlockHash(aBlock.index,aBlock.previousHash,aBlock.timestamp,aBlock.data)
+
+
+const isBlockValid = (candidateBlock:Block,previousBlock:Block):boolean=>{
+
+    if(!Block.validateStructure(candidateBlock)){
+        return false
+    }else if(previousBlock.index +1 !==candidateBlock.index){
+        return false
+    }else if(previousBlock.hash !== candidateBlock.previousHash)
+    {
+        return false
+    }
+    else if(getHashforBlock(candidateBlock) !==candidateBlock.hash){
+        return false
+    }else{
+        return true
+    }
+}
 
 export {}
